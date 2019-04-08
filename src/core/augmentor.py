2bd52e036a7e2b016a61f5bb6ed855bb09c8f7ae
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2019-04-08 22:21:52 +0700

    feat(core): Add augmentors

---
+++ src/core/augmentor.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+from util.tensorplow import Ops
+
+
+class VisionAugmentor(Ops):
+    def __init__(self, shape):
+        super().__init__()
+        self.shape = shape
+        self.frame = tf.placeholder(tf.dtypes.uint8, (None, None, 3))
+
+    def get_ops(self, frame):
+        crop = tf.image.random_crop(self.frame, self.shape)
+        brightness = tf.image.random_brightness(crop, 0.25)
+        saturation = tf.image.random_saturation(brightness, 0.5, 1.5)
+        flip = tf.image.random_flip_left_right(saturation)
+        return flip
+
+    def parse(self, frame):
+        return {self.frame: frame}
+
+
+class AudioAugmentor(Ops):
+    def __init__(self, shape):
+        super().__init__()
+        self.shape = shape
+        self.spectrogram = tf.placeholder(tf.dtypes.uint8, (None, None, 1))
+
+    def get_ops(self, spectrogram):
+        resize = tf.image.resize_images(self.spectrogram, self.shape)
+        return resize
+
+    def parse(self, spectrogram):
+        return {self.spectrogram: spectrogram}
