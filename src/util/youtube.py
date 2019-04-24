fcd4da5987912fa167fcb48bc7f8b8b067762517
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2019-04-24 17:23:44 +0700

    fix(youtube): Add 'token' to non-network related error keywords
    There was a new error returned by youtube-dl as per April 24th, 2019.
    This bug said to update to the latest youtube-dl version but there
    is no newer version of youtube-dl released at the moment. Therefore
    I add 'token' keyword to the list.

--- src/util/youtube.py
+++ src/util/youtube.py
@@ -5,7 +5,7 @@ url_template = 'https://youtube.com/watch'
 outfile_extensions = ('mkv', 'mp4', 'webm')
 
 # non-network keyword errors
-err_keywords = ['copyright', 'unavailable', 'account', 'Terms of Service', 'terminated', 'removed']
+err_keywords = ['copyright', 'unavailable', 'account', 'Terms of Service', 'terminated', 'removed', 'token']
 
 
 def dl(v, raise_exception=False, **options):
